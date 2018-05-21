e202c2d303d515012d58208e5e8c13e19c9c76af
Aaron Wilder <element1028@gmaill.com>
2018-05-21 00:11:34 -0700

    comments

--- src/App.js
+++ src/App.js
@@ -6,10 +6,13 @@ const App = () => {
     return (
       <div>
         <GoogleMapContainer/>
-        {/* <SimpleMap/> */}
+        {/* <SimpleMap/> */} 
         <h1>Hello</h1>
       </div>
     )
 }
-  
+  // To switch between each working map emplementations you'll
+  // need to comment one out and and uncomment the other
+  //  GoogleMapContainer for google-maps-react
+  // SimpleMap for google-map-react
 export default App;
